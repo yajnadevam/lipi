a359ead658f3c590cfce8d89dce96c65039d60b4
yajnadevam <yajnadevam@proton.me>
2024-09-09 17:19:57 -0500

    colored broken

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -24,7 +24,6 @@
         location="right"
         temporary
       >
-          <!-- prepend-avatar="https://randomuser.me/api/portraits/men/78.jpg" -->
         <v-list-item
           title="Allograph values"
         ></v-list-item>
@@ -41,8 +40,6 @@
         <v-divider></v-divider>
 
         <v-list density="compact" nav>
-          <!-- <v-list-item prepend-icon="mdi-view-dashboard" title="Home" value="home"></v-list-item>
-          <v-list-item prepend-icon="mdi-forum" title="About" value="about"></v-list-item> -->
           <Key />
         </v-list>
       </v-navigation-drawer>
@@ -64,6 +61,7 @@
         <div class="d-flex justify-center align-center h-100">
   <v-data-table
     v-model:expanded="expanded"
+    :row-props="itemrow"
     :custom-filter="filterInscriptions"
     :headers="computedHeaders"
     item-value="id"
@@ -71,17 +69,13 @@
     :search="search"
     show-expand
   >
-    <template v-slot:top>
+    <template #top>
       <v-text-field
         v-model="search"
         class="pa-2"
         label="Search Indus valley inscriptions"
-      ></v-text-field>
+      />
     </template>
-    <!-- <template v-slot:item="props">
-      <template v-if="!props.item.show">
-      </template>
-    </template> -->
 
     <template v-slot:expanded-row="{ columns, item }">
       <tr>
@@ -103,14 +97,9 @@
   import sanskrittransliterate from 'devanagari-transliterate'
   import incx from '../assets/data/inscriptions.csv?raw'
   import xlits from '../assets/data/xlits.csv?raw'
-  // import mw from '../assets/data/mw.txt?raw'
 
   const inx = csv2json(incx, { keys: ['id', 'cisi', 'site', 'complete', 'text', 'sanskrit', 'translation'] })
   const xlitarray = csv2json(xlits)
-  const borders = {
-    L: { ']': '\uE3E5', '+': '' }, //'\uE3E3' },
-    R: { '[': '\uE3E6', '+': '' }, //'\uE3E4' },
-  }
 
   const canonMap = {}
   const xlitmap = {}
@@ -135,22 +124,7 @@
     el.canonized = canonized.canon
     el.sanskrit = sanskrittransliterate('SLP', 'latin2devanagari', el.sanskrit)
               + '\n' + sanskrittransliterate('SLP', 'latin2ISO', el.sanskrit)
-    // const regex = new RegExp('^' + el.regex + '$', 'smg')
-    // const matches = mw.match(regex)
-    // el.best = matches === null ? '' : matches[0]
   })
-
-  // function jsize (text) {
-  //   text = text.trim()
-  //   const slices = text.split(/\//).reverse()
-  //   if (slices === null) return ''
-  //   const jslices = slices.map($item => { return jsizepart($item) })
-  //   const L = borders.L[text.charAt(0)]
-  //   const R = borders.R[text.charAt(text.length - 1)]
-  //   if (jslices[0]) jslices[0] = L + jslices[0]
-  //   if (jslices[jslices.length - 1]) jslices[jslices.length - 1] += R
-  //   return jslices.join('\n')
-  // }
 
   function canonized (text) {
     let canonizedStr = ''
@@ -188,7 +162,6 @@
         canon += thisChar
       }
     })
-    // if (canon.length > 20) console.log('Canonical of ', JSON.parse('"' + str + '"'), 'is', JSON.parse('"' + canon + '"'))
     str = JSON.parse('"' + str + '"')
     canon = JSON.parse('"' + canon + '"')
     return { str, canon }
@@ -262,9 +235,7 @@
           { title: 'CISI ID', key: 'cisi' },
           { title: 'Inscription', key: 'canonized', align: 'end', cellProps: { class: 'indus' } },
           { title: 'Transliteration', key: 'description', align: ' d-none' },
-          // { title: 'Regex', key: 'regex' },
           { title: 'Sanskrit', key: 'sanskrit', cellProps: { class: 'sanskrit' } },
-          // { title: 'IAST', key: 'iast' },
           { title: 'Translation', key: 'translation' },
           { title: '', key: 'data-table-expand' },
         ],
@@ -302,12 +273,11 @@
           (query.length > 0 && query.charCodeAt(0) >= 0xE000 && canonized(value).indexOf(canonized(query)) !== -1))
         )
       },
+      itemrow (item) {
+        return item.item.complete === 'Y' ? { class: 'text-white' } : { class: 'text-red' }
+      },
     },
   }
-
-  const filtered = computed(() =>
-    data.value.filter(e => e.raw.complete === 'Y')
-  )
 
 </script>
 
