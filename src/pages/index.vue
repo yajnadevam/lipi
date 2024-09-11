0e04f517603b72a2d605cc3505502fca0155a368
yajnadevam <yajnadevam@proton.me>
2024-09-11 00:50:05 -0500

    inline icons

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,7 +6,7 @@
     <v-menu :close-on-content-click="false" :close-delay="2000" :open-on-hover="true">
       <template v-slot:activator="{ props }">
       <v-btn  v-bind="props" icon>
-          <v-icon>mdi-cog-outline</v-icon>
+          <v-icon :icon="icons.cog"></v-icon>
         </v-btn>
       </template>
       <v-list style="padding: 4px;">
@@ -40,7 +40,7 @@
     </v-menu>
       
     <v-btn icon>
-        <v-icon>mdi-cog-outline</v-icon>
+        <v-icon :icon="activeFab.icon"></v-icon>
       </v-btn>
   </v-toolbar>
       <v-navigation-drawer
@@ -86,6 +86,10 @@
   <v-data-table
     v-model:expanded="expanded"
     :custom-filter="filterInscriptions"
+    :first-icon="icons.pageFirst"
+    :last-icon="icons.pageLast"
+    :next-icon="icons.chevRight"
+    :prev-icon="icons.chevLeft"
     :headers="computedHeaders"
     item-value="id"
     :items="filtered"
@@ -93,7 +97,7 @@
     :row-props="itemrow"
     :search="search"
     show-expand
-    @update:page="pageChange"
+      @update:page="pageChange"
   >
     <template #top>
       <v-text-field
@@ -101,6 +105,12 @@
         class="pa-2"
         label="Search Indus valley inscriptions"
       />
+    </template>
+
+    <template v-slot:item.data-table-expand="{item, isExpanded}">
+      <v-icon v-if="isExpandedRow(item.id)" :icon="icons.collapse" @click="handleExpansion(item, isExpanded)"></v-icon>
+      <v-icon v-else :icon="icons.expand" @click="handleExpansion(item, isExpanded)"></v-icon>
+
     </template>
 
     <template v-slot:expanded-row="{ columns, item }">
@@ -262,6 +272,15 @@
     components: { Key },
     data () {
       return {
+        icons: {
+          pageFirst: ['M18.41,16.59L13.82,12L18.41,7.41L17,6L11,12L17,18L18.41,16.59M6,6H8V18H6V6Z'],
+          pageLast: ['M5.59,7.41L10.18,12L5.59,16.59L7,18L13,12L7,6L5.59,7.41M16,6H18V18H16V6Z'],
+          chevLeft: ['M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z'],
+          chevRight: ['M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z'],
+          expand: ['M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z'],
+          collapse: ['M7.41,15.41L12,10.83L16.59,15.41L18,14L12,8L6,14L7.41,15.41Z'],
+          cog: ['M 12 15.5 A 3.5 3.5 0 0 1 8.5 12 A 3.5 3.5 0 0 1 12 8.5 A 3.5 3.5 0 0 1 15.5 12 A 3.5 3.5 0 0 1 12 15.5 M 19.43 12.97 C 19.47 12.65 19.5 12.33 19.5 12 C 19.5 11.67 19.47 11.34 19.43 11 L 21.54 9.37 C 21.73 9.22 21.78 8.95 21.66 8.73 L 19.66 5.27 C 19.54 5.05 19.27 4.96 19.05 5.05 L 16.56 6.05 C 16.04 5.66 15.5 5.32 14.87 5.07 L 14.5 2.42 C 14.46 2.18 14.25 2 14 2 H 10 C 9.75 2 9.54 2.18 9.5 2.42 L 9.13 5.07 C 8.5 5.32 7.96 5.66 7.44 6.05 L 4.95 5.05 C 4.73 4.96 4.46 5.05 4.34 5.27 L 2.34 8.73 C 2.21 8.95 2.27 9.22 2.46 9.37 L 4.57 11 C 4.53 11.34 4.5 11.67 4.5 12 C 4.5 12.33 4.53 12.65 4.57 12.97 L 2.46 14.63 C 2.27 14.78 2.21 15.05 2.34 15.27 L 4.34 18.73 C 4.46 18.95 4.73 19.03 4.95 18.95 L 7.44 17.94 C 7.96 18.34 8.5 18.68 9.13 18.93 L 9.5 21.58 C 9.54 21.82 9.75 22 10 22 H 14 C 14.25 22 14.46 21.82 14.5 21.58 L 14.87 18.93 C 15.5 18.67 16.04 18.34 16.56 17.94 L 19.05 18.95 C 19.27 19.03 19.54 18.95 19.66 18.73 L 21.66 15.27 C 21.78 15.05 21.73 14.78 21.54 14.63 L 19.43 12.97 Z'],
+        },
         search: '',
         drawer: null,
         pageNum: 1,
@@ -283,8 +302,8 @@
     },
     computed: {
       activeFab () {
-        return this.drawer ? { color: 'white', icon: 'mdi-chevron-right' }
-          : { color: 'green', icon: 'mdi-chevron-left' }
+        return this.drawer ? { color: 'white', icon: this.icons.chevRight }
+          : { color: 'green', icon: this.icons.chevLeft }
       },
       filtered () {
         return this.items.filter(e => this.optionBroken || e.complete === 'Y')
@@ -300,6 +319,16 @@
     },
 
     methods: {
+      chevLeft () { return this.icons.chevLeft },
+      chevRight () { return this.icons.chevRight },
+      isExpandedRow (id) {
+        return this.expanded.indexOf(id) > -1
+      },
+      handleExpansion (item, state) {
+        const itemIndex = this.expanded.indexOf(item.id)
+
+        itemIndex > -1 ? this.expanded.splice(itemIndex, 1) : this.expanded.push(item.id)
+      },
       filterInscriptions (value, query, item) {
         return (
           value != null &&
@@ -337,7 +366,7 @@
 <style>
         @font-face {
         font-family: 'indus_scriptregular';
-        src: url('../assets/fonts/indus-font.ttf') format('truetype');
+        src: url('../assets/fonts/indus-font.woff2') format('woff2');
         font-weight: normal;
         font-style: normal;
         font-size: 24pt;
