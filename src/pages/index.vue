1d2eb62c36fa966f88fd4b2292953e397802ec5e
yajnadevam <yajnadevam@proton.me>
2024-09-25 01:42:43 -0500

    add search by id

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -367,9 +367,11 @@
           value != null &&
           query != null &&
           ((this.optionBroken && item.raw.complete === 'N') || item.raw.complete === 'Y') &&
-          typeof value === 'string' &&
-          ((value.toString().toLocaleLowerCase().indexOf(query.toLocaleLowerCase()) !== -1) ||
-          (query.length > 0 && query.charCodeAt(0) >= 0xE000 && canonized(value).indexOf(canonized(query)) !== -1))
+          (typeof value === 'string' || typeof value === 'number') &&
+          (value === query ||
+             (value.toString().toLocaleLowerCase().indexOf(query.toLocaleLowerCase()) !== -1) ||
+             (query.length > 0 && query.charCodeAt(0) >= 0xE000 && canonized(value).indexOf(canonized(query)) !== -1)
+          )
         )
       },
       itemrow (item) {
