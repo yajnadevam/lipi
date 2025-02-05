938bc182103fc9f81db7472ffdfdd1caa3fd86f7
Vedant Madane <6527493+VedantMadane@users.noreply.github.com>
2025-02-05 15:12:34 +0530

    Delete src/utils/splash.js

--- src/utils/splash.js
+++
@@ -1,6 +0,0 @@
-export function hideSplashScreen() {
-  setTimeout(function () {
-    const splashScreen = document.querySelector(".splash");
-    if (splashScreen) splashScreen.classList.add("hidden");
-  }, 100);
-}
