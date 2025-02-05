35af121e7a45d4d580141ab6635f5bc8aff38988
Vedant Madane <6527493+VedantMadane@users.noreply.github.com>
2025-02-05 10:17:51 +0000

    Remove splash.js import from App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
   <v-app>
     <v-main>
-      <router-view></router-view>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts" setup>
-import { onMounted } from 'vue'
-import { hideSplashScreen } from '@/utils/splash.js'
+  //
+  onMounted(hook)
 
-onMounted(() => {
-  hideSplashScreen();
-});
+  function hook () {
+  }
 </script>
